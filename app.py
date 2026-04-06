4c4e8cb875546571edb4683d6d4b8c3fa1fb81fc
Pomerania <jordan.pommerenck@gmail.com>
2026-04-06 16:43:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -129,7 +129,6 @@ L = st.sidebar.number_input("Distance to Screen (L) [m]", value=0.7, step=0.1)
 exposure = st.sidebar.slider("Visual Exposure (Gain)", 0.01, 1.0, 0.2)
 
 # --- NEW: Student Math Check Inputs ---
-st.sidebar.divider()
 st.sidebar.header("4. Math Check")
 if mode == "Single Slit":
     student_val = st.sidebar.number_input("Your Calculated 'a' [μm]", value=0.0, step=0.1)
